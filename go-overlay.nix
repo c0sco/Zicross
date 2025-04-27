e519374291a372ed1e9ce5d2b401bb467c5b06d0
Matt Stofko <matt@mjslabs.com>
2025-04-27 15:06:22 -0700

    Updating golang version; autoformatting

--- go-overlay.nix
+++ go-overlay.nix
@@ -5,34 +5,54 @@ final: prev: {
     # target architecture, or null for native
     GOARCH ? null,
     ...
-  }@args': ((prev.buildGo119Module.override {
-    go = prev.go_1_19 // {
-      GOOS = if GOOS == null then prev.go_1_19.GOOS else GOOS;
-      GOARCH = if GOARCH == null then prev.go_1_19.GOARCH else GOARCH;
+  } @ args':
+    ((prev.buildGo124Module.override {
+        go =
+          prev.go_1_24
+          // {
+            GOOS =
+              if GOOS == null
+              then prev.go_1_24.GOOS
+              else GOOS;
+            GOARCH =
+              if GOARCH == null
+              then prev.go_1_24.GOARCH
+              else GOARCH;
+            CGO_ENABLED = true;
+          };
+      })
+      args').overrideAttrs (origAttrs: {
       CGO_ENABLED = true;
-    };
-  }) args').overrideAttrs (origAttrs: {
-    CGO_ENABLED = true;
-    configurePhase = origAttrs.configurePhase + ''
-      # requires zigStdenv from zig-overlay
-      export CC=${prev.zig}/bin/cc
-      export LD=${prev.zig}/bin/cc
-      export NIX_CFLAGS_COMPILE=
-      export NIX_LDFLAGS=
-      
-      ${if prev.stdenv.isDarwin then ''
-        buildFlags="$buildFlags -buildmode=pie"
-        ldflags="$ldflags -s -w"
-      '' else ""}
-    '';
-    postConfigure = (origAttrs.postConfigure or "") + ''
-      export CGO_CFLAGS="$CFLAGS -Wno-expansion-to-defined -Wno-nullability-completeness"
-    '';
-    buildPhase = origAttrs.buildPhase + ''
-      if ! [ -z ''${ZIG_TARGET+x} ]; then
-        mv $GOPATH/bin/''${GOOS}_$GOARCH/* $GOPATH/bin
-        rmdir $GOPATH/bin/''${GOOS}_$GOARCH
-      fi
-    '';
-  });
+      configurePhase =
+        origAttrs.configurePhase
+        + ''
+          # requires zigStdenv from zig-overlay
+          export CC=${prev.zig}/bin/cc
+          export LD=${prev.zig}/bin/cc
+          export NIX_CFLAGS_COMPILE=
+          export NIX_LDFLAGS=
+
+          ${
+            if prev.stdenv.isDarwin
+            then ''
+              buildFlags="$buildFlags -buildmode=pie"
+              ldflags="$ldflags -s -w"
+            ''
+            else ""
+          }
+        '';
+      postConfigure =
+        (origAttrs.postConfigure or "")
+        + ''
+          export CGO_CFLAGS="$CFLAGS -Wno-expansion-to-defined -Wno-nullability-completeness"
+        '';
+      buildPhase =
+        origAttrs.buildPhase
+        + ''
+          if ! [ -z ''${ZIG_TARGET+x} ]; then
+            mv $GOPATH/bin/''${GOOS}_$GOARCH/* $GOPATH/bin
+            rmdir $GOPATH/bin/''${GOOS}_$GOARCH
+          fi
+        '';
+    });
 }
